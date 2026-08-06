66910dd967a0d314e32145ba52d68be71a7ebfd1
Matar <143889648+Mata4r@users.noreply.github.com>
2026-08-06 23:32:24 +0300

    Refactor host_scan.py for improved readability

--- scripts/host_scan.py
+++ scripts/host_scan.py
@@ -27,7 +27,6 @@ def start_host_scan(Target,
     lookup = MacLookup()
     
     if answered:
-        print()
         print(f"-Initiating Ncache scan\n-Ncache scan report for {Target}\n")
         console.print("IP Address\tMAC Address\t\tVendor", style="italic magenta")
         seen = set()
